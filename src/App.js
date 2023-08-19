50cebd2ebd0300994221404ef1e56097faea2084
Ahmet Cigil <ahmetcgln@gmail.com>
2023-08-19 17:57:44 +0200

    Routes completed

--- src/App.js
+++ src/App.js
@@ -4,14 +4,12 @@ import { Box } from "@mui/material";
 const App = () => (
 	<BrowserRouter>
 		<Box sx={{ backgroundColor: "#FFF" }}>
-			Navbar
+			{/* Navbar */}
 			<Routes>
 				<Route path="/" exact element={<Feed />} />
 				<Route path="/video/:id" element={<VideoDetail />} />
-				<Route path="/channel/:id" exact element={<ChannelDetail />} />
-				<Route path="/search/:searchTerm" exact element={<Feed />} />
-				<Route path="/" exact element={<Feed />} />
-				<Route path="/" exact element={<Feed />} />
+				<Route path="/channel/:id" element={<ChannelDetail />} />
+				<Route path="/search/:searchTerm" element={<Searchfeed />} />
 			</Routes>
 		</Box>
 	</BrowserRouter>
