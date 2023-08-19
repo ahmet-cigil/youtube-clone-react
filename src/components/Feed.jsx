ad7d619653446bd75e57baaa8144ef3b3ce7d2c2
Ahmet Cigil <ahmetcgln@gmail.com>
2023-08-19 23:06:15 +0200

    fetch func and videos are created

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,9 +1,21 @@
 import React, { useEffect, useState } from "react";
 import { Box, Stack, Typography } from "@mui/material";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 import { Videos, Sidebar } from "./";
 
 const Feed = () => {
+	const [selectedCategory, setSelectedCategory] = useState("New");
+	const [videos, setVideos] = useState([]);
+
+	useEffect(() => {
+		fetchFromAPI(`search?part=snippet&q=${{ selectedCategory }}`).then(
+			(data) => {
+				setVideos(data.items);
+			}
+		);
+	}, [selectedCategory]);
+
 	return (
 		<Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
 			<Box
@@ -13,7 +25,10 @@ const Feed = () => {
 					px: { sx: 0, md: 2 },
 				}}
 			>
-				<Sidebar />
+				<Sidebar
+					selectedCategory={selectedCategory}
+					setSelectedCategory={setSelectedCategory}
+				/>
 				<Typography
 					className="copyright"
 					variant="body2"
@@ -39,9 +54,9 @@ const Feed = () => {
 					md={2}
 					sx={{ color: "white" }}
 				>
-					New <span style={{ color: "#F31503" }}>videos</span>
+					{selectedCategory} <span style={{ color: "#F31503" }}>videos</span>
 				</Typography>
-				<Videos />
+				<Videos videos={videos} />
 			</Box>
 		</Stack>
 	);
---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube-v31.p.rapidapi.com";
+
+const options = {
+	url: BASE_URL,
+	params: {
+		maxResults: "50",
+	},
+	headers: {
+		"X-RapidAPI-Key": "b34eb2bc11mshc91836324ed7789p143e08jsn8a272a8773b7",
+		"X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
+	},
+};
+
+try {
+	const response = await axios.request(options);
+	console.log(response.data);
+} catch (error) {
+	console.error(error);
+}
+
+export const fetchFromAPI = async (url) => {
+	const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+	return data;
+};
